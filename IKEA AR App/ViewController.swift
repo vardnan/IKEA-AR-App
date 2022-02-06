bb35372cf4da13df257fdedb3edd735a50fea47f
Vardnan <46565659+vardnan@users.noreply.github.com>
2022-02-06 13:46:45 +0000

    initial commit

--- IKEA AR App/ViewController.swift
+++ IKEA AR App/ViewController.swift
@@ -6,14 +6,148 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, ARSCNViewDelegate {
+    
+    let itemsArray: [String] = ["cup", "vase", "boxing", "table"]
+    
+    @IBOutlet weak var planeDetected: UILabel!
+    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var itemsCollectionView: UICollectionView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    var selectedItem: String?
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.configuration.planeDetection = .horizontal
+        self.sceneView.session.run(configuration)
+        self.itemsCollectionView.dataSource = self
+        self.itemsCollectionView.delegate = self
+        self.sceneView.delegate = self
+        self.registerGestureRecognizers()
+        self.sceneView.autoenablesDefaultLighting = true
         // Do any additional setup after loading the view.
     }
+    
+    func registerGestureRecognizers() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(pinch))
+        let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(rotate))
+        longPressGestureRecognizer.minimumPressDuration = 0.1
+        self.sceneView.addGestureRecognizer(longPressGestureRecognizer)
+        self.sceneView.addGestureRecognizer(pinchGestureRecognizer)
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func rotate(sender: UILongPressGestureRecognizer) {
+        let sceneView = sender.view as! ARSCNView
+        let holdLocation = sender.location(in: sceneView)
+        let hitTest = sceneView.hitTest(holdLocation)
+        if !hitTest.isEmpty {
+            let results = hitTest.first!
+            
+            if sender.state == .began {
+                let rotateAction = SCNAction.rotateBy(x: 0, y: 360.degreesToRadians, z: 0, duration: 1)
+                let forever = SCNAction.repeatForever(rotateAction)
+                results.node.runAction(forever)
+                
+            } else if sender.state == .ended {
+                results.node.removeAllActions()
+            }
+            
+        }
+        
+        
+        
+    }
+    
+    @objc func pinch(sender: UIPinchGestureRecognizer) {
+        let sceneView = sender.view as! ARSCNView
+        let pinchLocation = sender.location(in: sceneView)
+        let hitTest = sceneView.hitTest(pinchLocation)
+        
+        if !hitTest.isEmpty {
+            
+            let results = hitTest.first!
+            let node = results.node
+            let pinchAction = SCNAction.scale(by: sender.scale, duration: 0)
+            node.runAction(pinchAction)
+            sender.scale = 1.0
+        }
+        
+    }
+    
+    @objc func tapped(sender: UITapGestureRecognizer) {
+        let sceneView = sender.view as! ARSCNView
+        let tapLocation = sender.location(in: sceneView)
+        let query = sceneView.raycastQuery(from: tapLocation, allowing: .existingPlaneGeometry, alignment: .horizontal)
+        guard let result = sceneView.session.raycast(query!).first else { return }
+        if query != nil {
+            self.addItem(queryResult: result)
+        }
+        
+    }
+    
+    func addItem(queryResult: ARRaycastResult) {
+        if let selectedItem = self.selectedItem {
+            let scene = SCNScene(named: "Models/\(selectedItem).scn")
+            let node = (scene?.rootNode.childNode(withName: selectedItem, recursively: false))!
+            let transform = queryResult.worldTransform
+            let thirdColumn = transform.columns.3
+            node.position = SCNVector3(thirdColumn.x, thirdColumn.y, thirdColumn.z)
+            if selectedItem == "table" {
+                self.centerPivot(for: node)
+            }
+            self.sceneView.scene.rootNode.addChildNode(node)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath) as! itemCell
+        cell.itemLabel.text = self.itemsArray[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        self.selectedItem = itemsArray[indexPath.row]
+        cell?.backgroundColor = UIColor.green
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.orange
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARPlaneAnchor else {return}
+        DispatchQueue.main.async {
+            self.planeDetected.isHidden = false
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                self.planeDetected.isHidden = true
+            }
+        }
+    }
+    
+    func centerPivot(for node: SCNNode) {
+        let min = node.boundingBox.min
+        let max = node.boundingBox.max
+        node.pivot = SCNMatrix4MakeTranslation(
+            min.x + (max.x - min.x)/2,
+            min.y + (max.y - min.y)/2,
+            min.z + (max.z - min.z)/2
+        )
+    }
+}
 
-
+extension Int {
+    
+    var degreesToRadians: Double { return Double(self) * .pi/100}
 }
 
---
+++ IKEA AR App/itemCell.swift
@@ -0,0 +1,13 @@
+//
+//  itemCell.swift
+//  IKEA AR App
+//
+//  Created by Vardnan Sivarajah on 03/02/2022.
+//
+
+import UIKit
+
+class itemCell: UICollectionViewCell {
+    @IBOutlet weak var itemLabel: UILabel!
+    
+}
